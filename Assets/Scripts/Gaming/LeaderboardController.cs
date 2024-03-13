81c65b4adbae4307fc8950cb43c2832ec71f52ed
小威 紀 <st93111233@gmail.com>
2024-03-14 00:45:23 +0800

    添加新分數和檢查玩家名稱是否存在的功能

--- Assets/Scripts/Gaming/LeaderboardController.cs
+++ Assets/Scripts/Gaming/LeaderboardController.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Ling;
 
 public class LeaderboardController : MonoBehaviour
 {
+    private List<PlayerScore> scores = new List<PlayerScore>();
+
     public void AddNewScore(string name, int score)
     {
-        throw new System.NotImplementedException();
+        // 檢查檢查該名稱是否已經存在於排行榜中
+        if (!NameExists(name))
+        {
+            scores.Add(new PlayerScore(name, score));
+
+            // 對分數進行排序
+            scores = scores.OrderByDescending(s => s.score).ToList();
+        }
+        else //如果名子存在，會更新數值
+        {   
+            var existingScore = scores.Find(s => s.name == name);
+            if (score > existingScore.score)
+            {
+                existingScore.score = score;
+            }
+         }
+
+    }   
+
+    public bool NameExists(string name)
+    {
+        return scores.Any(s => s.name == name);
     }
-    public bool NameExists(string name){
-        throw new System.NotImplementedException();
-    }
-}
+
+
