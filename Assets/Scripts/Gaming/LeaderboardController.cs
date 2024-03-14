1ec2c28df9ee3622da4a50b9b2bdd90c297a6ab7
小威 紀 <st93111233@gmail.com>
2024-03-14 15:56:40 +0800

    Renew LeadboardController

--- Assets/Scripts/Gaming/LeaderboardController.cs
+++ Assets/Scripts/Gaming/LeaderboardController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Ling;
+using System.Linq;
 
 public class LeaderboardController : MonoBehaviour
 {
+
     private List<PlayerScore> scores = new List<PlayerScore>();
 
     public void AddNewScore(string name, int score)
@@ -15,22 +16,34 @@ public class LeaderboardController : MonoBehaviour
             scores.Add(new PlayerScore(name, score));
 
             // Sort scores
-            scores = scores.OrderByDescending(s => s.score).ToList();
+            scores = scores.OrderByDescending(s => s.Score).ToList();
         }
         else //If the name already exists
-        {   
-            var existingScore = scores.Find(s => s.name == name);
-            if (score > existingScore.score)
+        {
+            var existingScore = scores.Find(s => s.Name == name);
+            if (score > existingScore.Score)
             {
-                existingScore.score = score;
+                existingScore.Score = score;
             }
-         }
+        }
 
-    }   
+    }
 
     public bool NameExists(string name)
     {
-        return scores.Any(s => s.name == name);
+        return scores.Any(s => s.Name == name);
     }
 
+    public class PlayerScore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public PlayerScore(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
 
