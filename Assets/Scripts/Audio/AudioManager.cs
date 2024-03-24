b5af0f47efa4304f31af4742de8a84a4f8de820d
Duke1213 <160474285+dukee1213@users.noreply.github.com>
2024-03-24 08:28:23 +0800

    add component
    sound class doesn't appear in sidebar of audiomanager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,3 +9,92 @@ public class AudioManager : Singleton<AudioManager>
         //throw new System.NotImplementedException();
     }
 }
+*/
+
+// no singleton
+
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance; // replace singleton
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {   // avoid rep during scene switch
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volumn;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Misspelling of the audio " + name + "!");
+            return;
+        }
+        s.source.Play();
+    }
+    void Start()
+    {
+        Play("background");
+    }
+}
+
+// if you gonna implement a singleton, try the following
+
+/*
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Sound[] sounds;
+    // public static AudioManager instance; // replace singleton
+    void Awake()
+    {
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volumn;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Misspelling of the audio " + name + "!");
+            return;
+        }
+        s.source.Play();
+    }
+    void Start()
+    {
+        Play("background");
+    }
+}
+*/
--- Assets/Scripts/Basic/Singleton.cs
+++ Assets/Scripts/Basic/Singleton.cs
@@ -1,3 +1,30 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get { return instance; }
+        set { instance = value; }
+    }
+
+    protected virtual void Awake()
+    // public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,3 +44,4 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         Instance = _instance;
     }
 }
+*/
